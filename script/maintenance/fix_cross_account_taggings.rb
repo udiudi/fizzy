461ff75b48160022df1a3f0806df5514729e9db2
Jorge Manrubia <jorge@hey.com>
2025-12-03 23:53:18 +0100

    Fix path

--- script/maintenance/fix_cross_account_taggings.rb
+++ script/maintenance/fix_cross_account_taggings.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-require_relative "../config/environment"
+require_relative "../../config/environment"
 
 cross_account_taggings = Tagging.joins(:tag).where("taggings.account_id != tags.account_id")
 
@@ -9,7 +9,7 @@ puts "Found #{cross_account_taggings.count} cross-account taggings to fix"
 cross_account_taggings.find_each do |tagging|
   correct_tag = tagging.account.tags.find_or_create_by!(title: tagging.tag.title)
   tagging.update!(tag: correct_tag)
-  puts "Fixed tagging #{tagging.id}: reassigned to tag #{correct_tag.id} (#{correct_tag.title})"
+  puts "Fixed tagging #{tagging.id}: reassigned to tag #{correct_tag.id}"
 end
 
 puts "Done!"
