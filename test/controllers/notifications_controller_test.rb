d49ffa803f075a51344084ab2f278cf8ea10039b
vkagithala <vkagithala@cirm.ca.gov>
2025-12-03 17:21:50 -0800

    Added 'Back to Home' link on notifications list page to improve navigation experience.

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class NotificationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index and show back button" do
+    sign_in_as users(:david)
+    get notifications_url
+    assert_response :success
+    assert_select "a[href=?]", root_path do
+      assert_select "strong", text: "Back to Home"
+    end
+  end
 end
