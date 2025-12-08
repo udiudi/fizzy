d1cda4c2ba2fbd69f1b210843d49456102cf8d15
Udi <udi@udi.codes>
2025-12-08 10:54:59 +0200

    A fix to prevent a cross-user vulnerability in a magic link

--- app/controllers/sessions/magic_links_controller.rb
+++ app/controllers/sessions/magic_links_controller.rb
@@ -10,7 +10,7 @@ class Sessions::MagicLinksController < ApplicationController
 
   def create
     if magic_link = MagicLink.consume(code)
-      authenticate_with(magic_link)
+      authenticate_with magic_link
     else
       redirect_to session_magic_link_path, flash: { shake: true }
     end
@@ -20,7 +20,7 @@ class Sessions::MagicLinksController < ApplicationController
     def authenticate_with(magic_link)
       if magic_link.identity.email_address == session[:pending_auth_email]
         session.delete(:pending_auth_email)
-        start_new_session_for(magic_link.identity)
+        start_new_session_for magic_link.identity
         redirect_to after_sign_in_url(magic_link)
       else
         redirect_to new_session_path, alert: "Authentication failed. Please try again."
